3dd90bbd28b67e803f2382b2be0b28ec7ebd2437
Joseph Crowe <joseph.crowe@live.co.uk>
2012-12-18 22:22:13 +0000

    Improved build process to always build from original files.

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-javac -cp newsrc -d newbin newsrc/mineshafter/proxy/MineProxy.java
+cd `dirname $0` && \
+rm -rf newbin/* && \
+javac -cp newsrc -d newbin newsrc/mineshafter/proxy/MineProxy.java && \
+rm -rf dist && \
+cp -R olddist dist && \
 jar uf dist/fto_client.jar -C newbin mineshafter
