28648d82a78cf05cfafe5707901389fe0f6f525d
Joseph Crowe <joseph.crowe@live.co.uk>
2012-12-19 00:41:44 +0000

    Changed to compile for Java 1.6 instead of the default of (at this time) 1.7.

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd `dirname $0` && \
 rm -rf newbin/* && \
-javac -cp newsrc -d newbin newsrc/mineshafter/proxy/MineProxy.java && \
+javac -source 6 -target 6 -cp newsrc -d newbin newsrc/mineshafter/proxy/MineProxy.java && \
 rm -rf dist && \
 cp -R olddist dist && \
 jar uf dist/fto_client.jar -C newbin mineshafter
